b771229bb28965a1d08d5358f479f05b03e33e8f
tuds <twilkinson573@gmail.com>
2022-04-08 12:26:00 +0100

    WIP setting up wETH & V2Router02 in run script

--- scripts/run.js
+++ scripts/run.js
@@ -36,51 +36,70 @@ const main = async () => {
   console.log('DAI total supply: ', await dai.totalSupply());
   console.log(' ');
 
+  console.log('####### Setting up & topping up wETH for testing \n');
+  const Weth = await hre.ethers.getContractFactory("WETH9");
+  const weth = await Weth.deploy();
+  await weth.deployed();
+  await weth.deposit{value: msg.sender.balance / 2}();
+
+  console.log('wETH deployed to: ', weth.address);
+  console.log('wETH total supply: ', await weth.totalSupply());
+  console.log(' ');
+
+
   // Deploy & configure the factory contract
   console.log('####### Deploying V2 Core (StarSeeds AMM Factory) \n')
 
   const Factory = await hre.ethers.getContractFactory("UniswapV2Factory");
-  const fct = await Factory.deploy(deployer.address);
-  await fct.deployed();
-  console.log('Factory deployed to: ', fct.address);
+  const factory = await Factory.deploy(deployer.address);
+  await factory.deployed();
+  console.log('Factory deployed to: ', factory.address);
   console.log(' ');
 
-  console.log('Factory fees accrue to: ', await fct.feeTo());
+  console.log('Factory fees accrue to: ', await factory.feeTo());
 
   console.log('Configuring feeTo address to collect fees...')
-  await fct.setFeeTo(deployer.address);
-  console.log('Factory fees accrue to: ', await fct.feeTo());
+  await factory.setFeeTo(deployer.address);
+  console.log('Factory fees accrue to: ', await factory.feeTo());
   console.log(' ');
+
+  const Router = await hre.ethers.getContractFactory("UniswapV2Router02");
+  const router = await Router.deploy(factory.address, weth.address)
+  await router.deployed();
+
+  console.log('Router deployed to: ', router.address);
+  console.log(' ');
+ 
 
   // NOTE - The following (rightfully) won't work due to the Factory's permissions
   // console.log('Illegal attempt to change fee collection address (should fail)...')
-  // const bobSetFeeToTxn = await fct.connect(bob).setFeeTo(bob.address);
+  // const bobSetFeeToTxn = await factory.connect(bob).setFeeTo(bob.address);
   // await bobSetFeeToTxn.wait();
 
   // Call Factory#createPair to instantiate a few trading pairs
-  console.log("Creating trading pairs")
+  console.log("####### Creating trading pairs")
   console.log('Create USDC / MATIC trading pair...')
-  await fct.createPair(usdc.address, matic.address);
-  console.log('USDC / MATIC trading pair deployed to: ', await fct.allPairs(0));
+  await factory.createPair(usdc.address, matic.address);
+  console.log('USDC / MATIC trading pair deployed to: ', await factory.allPairs(0));
 
   console.log('Create DAI / MATIC trading pair...')
-  await fct.createPair(dai.address, matic.address);
-  console.log('DAI / MATIC trading pair deployed to: ', await fct.allPairs(1));
+  await factory.createPair(dai.address, matic.address);
+  console.log('DAI / MATIC trading pair deployed to: ', await factory.allPairs(1));
 
   console.log('Create USDC / DAI trading pair...')
-  await fct.createPair(usdc.address, dai.address);
-  console.log('USDC / DAI trading pair deployed to: ', await fct.allPairs(2));
+  await factory.createPair(usdc.address, dai.address);
+  console.log('USDC / DAI trading pair deployed to: ', await factory.allPairs(2));
 
   // NOTE - The following (rightfully) won't work due to pair existing protections
   // console.log('Attempting to create duplicate MATIC / USDC trading pair (should fail)...')
-  // await fct.createPair(matic.address, usdc.address);
+  // await factory.createPair(matic.address, usdc.address);
 
-  console.log('Amount of trading pairs #: ', await fct.allPairsLength());
+  console.log('Amount of trading pairs #: ', await factory.allPairsLength());
 
   // NOTE This is currently failing (correctly) due to insufficient liquidity (as none has been provided yet)
   // Just a sanity check that everything is working - we will make swaps below using V2Router02
   // console.log('Making test swap')
-  // const pair1 = await hre.ethers.getContractAt("UniswapV2Pair", await fct.allPairs(0));
+  // const pair1 = await hre.ethers.getContractAt("UniswapV2Pair", await factory.allPairs(0));
   // await pair1.swap(10, 10, bob.address, "0x00");
 
 
